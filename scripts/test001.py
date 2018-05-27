fe88f823115fc2182e167eed1e14fb652f11815f
xzyxcm <xcmxzy@126.com>
2018-05-27 16:18:40 +0800

    测试步骤

--- scripts/test001.py
+++ scripts/test001.py
@@ -1,7 +1,7 @@
-import pytest
+import pytest,allure
 
 class Test_001:
-
+    @allure.step(title="测试步骤001")
     @pytest.mark.parametrize("a",[1,2,3])
     def test_adc(self,a):
         assert a!=2
